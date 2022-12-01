8a13fc9b6ab490408d1f59f0109a7666127ad508
leksts <leks@leksts.com>
2022-12-01 10:16:08 +0100

    Initial

---
+++ Demo.cs
@@ -0,0 +1,40 @@
+﻿namespace NullRefTypesDemo;
+
+public class Consumer
+{
+    public void DoWork()
+    {
+        Dto dto = GetDto();
+        Service service = new(dto);
+    }
+
+    private Dto GetDto()
+    {
+        if (new Random().Next(0, 1) == 1)
+        {
+            return new Dto { Value1 = 1, Value2 = 2 };
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+internal class Service
+{
+    int value1;
+    int value2;
+
+    public Service(Dto dto)
+    {
+        value1 = dto.Value1;
+        value2 = dto.Value2;
+    }
+}
+
+public class Dto
+{
+    public int Value1 { get; set; }
+    public int Value2 { get; set; }
+}
